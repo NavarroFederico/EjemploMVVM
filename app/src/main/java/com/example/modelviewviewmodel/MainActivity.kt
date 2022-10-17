f2bb95efc3de552db57a7b8810432196d7afad01
Federico Navarro <navarrofederico14@gmail.com>
2022-10-16 23:18:37 -0300

    commit initial

--- app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.modelviewviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.modelviewviewmodel.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var TAG="ActividadPrincipal"
     private lateinit var binding: ActivityMainBinding
 
     private var count:Int = 0
@@ -14,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        Log.d(TAG,"On create...")
         btn_plus.setOnClickListener{
             count++
             tv_count.text = count.toString()
@@ -23,6 +25,11 @@ class MainActivity : AppCompatActivity() {
             count--
             tv_count.text = count.toString()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"On Destroy...")
     }
 
 
