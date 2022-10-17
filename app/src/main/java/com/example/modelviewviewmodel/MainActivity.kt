060ef548209a1cd4d5f5bca0ed11ba92ea071b1f
Federico Navarro <navarrofederico14@gmail.com>
2022-10-16 21:24:40 -0300

    Initial commit

---
+++ app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.modelviewviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.modelviewviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var count:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
