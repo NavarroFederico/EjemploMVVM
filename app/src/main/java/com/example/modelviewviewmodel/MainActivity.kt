01be760cefe36bbd7da12f2bc368959fc60fcbaa
Federico Navarro <navarrofederico14@gmail.com>
2022-10-16 23:57:43 -0300

    Se agrego el Tag para el logeo

--- app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.modelviewviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import com.example.modelviewviewmodel.databinding.ActivityMainBinding
 import com.example.modelviewviewmodel.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val tag = "ActividadPrincipal"
     private lateinit var binding: ActivityMainBinding
 
     private var count:Int = 0
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        Log.d(tag,"OnCreate de la actividad")
         btn_plus.setOnClickListener{
             count++
             tv_count.text = count.toString()
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         }
         btn_finish.setOnClickListener{
             finish()
+            Log.d(tag,"onFinish de la actividad")
         }
         viewModel
     }
