6ac65200aa206fa52ff1eefe63535f73130c8935
Federico Navarro <navarrofederico14@gmail.com>
2022-10-16 22:54:53 -0300

    Dependencias y contador funcionando

--- app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.modelviewviewmodel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.modelviewviewmodel.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        btn_plus.setOnClickListener{
+            count++
+            tv_count.text = count.toString()
+        }
+        btn_minus.setOnClickListener{
+            count--
+            tv_count.text = count.toString()
+        }
     }
+
+
 }
