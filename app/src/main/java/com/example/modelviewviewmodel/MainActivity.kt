0a2f93516ffe692fea7acb85a3b28545c1d6bb07
Federico Navarro <navarrofederico14@gmail.com>
2022-10-16 23:39:07 -0300

    Creacion de ViewModel

--- app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.modelviewviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.modelviewviewmodel.databinding.ActivityMainBinding
+import com.example.modelviewviewmodel.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private var count:Int = 0
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,8 @@ class MainActivity : AppCompatActivity() {
             count--
             tv_count.text = count.toString()
         }
+
+        viewModel
     }
 
 
---
+++ app/src/main/java/com/example/modelviewviewmodel/viewmodels/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.modelviewviewmodel.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+/*
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("ActividadPrincipal", "On cleared...Se finaliza el ciclo de vida del viewModel")
+    }*/
+}
