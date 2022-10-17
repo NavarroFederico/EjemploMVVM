49e75c8b774e0f00ee26932540cb1984807c8925
Federico Navarro <navarrofederico14@gmail.com>
2022-10-17 10:21:17 -0300

    contador descendente finalizado

--- app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/modelviewviewmodel/MainActivity.kt
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.modelviewviewmodel.databinding.ActivityMainBinding
 import com.example.modelviewviewmodel.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 
 class MainActivity : AppCompatActivity() {
     val tag = "ActividadPrincipal"
     private lateinit var binding: ActivityMainBinding
 
-    private var count:Int = 0
+    private var count: Int = 0
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -20,20 +25,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d(tag,"OnCreate de la actividad")
+        Log.d(tag, "OnCreate de la actividad")
+
+        //collect es una funcion suspendible por eso vamos a ejecutarla dentro de una corrutina
+        lifecycleScope.launchWhenStarted {
+            viewModel.time.collect { value ->//tiene que colectar
+                binding.tvCount.text = value.toString()
+            }
+
+        }
 
 
-
-        btn_finish.setOnClickListener{
+        btn_finish.setOnClickListener {
             finish()
-            Log.d(tag,"onFinish de la actividad")
+            Log.d(tag, "onFinish de la actividad")
         }
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(tag,"On destroy de la actividad")
+        Log.d(tag, "On destroy de la actividad")
 
     }
 
--- app/src/main/java/com/example/modelviewviewmodel/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/modelviewviewmodel/viewmodels/MainViewModel.kt
@@ -2,8 +2,10 @@ package com.example.modelviewviewmodel.viewmodels
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
 /*
 //utilizamos un StateFlow es propio de las corrutinas es propio de Kotlin
 //Kotlin nos permite que sea multiplataforma
@@ -12,14 +14,19 @@ import kotlinx.coroutines.flow.StateFlow
 class MainViewModel : ViewModel() {
 
 
-    private var _count= MutableStateFlow<Int>(0)//la diferencia con el LiveData es que nos pide un valor inicial y por lo tanto no va necesitar de un bloque Init
-    val count: StateFlow<Int> = _count
+    private var _time = MutableStateFlow<Int>(0)//la diferencia con el LiveData es que nos pide un valor inicial y por lo tanto no va necesitar de un bloque Init
+    val time: StateFlow<Int> = _time
 
+    init {
+        startTime()
+    }
 
-
-
-
-
+    private fun startTime() {
+        (60 downTo 0).asFlow().onEach { value ->
+            _time.value = value
+            delay(1000)
+        }.launchIn(viewModelScope) //le indico en que Scope se va a correr
+    }
 
 
     override fun onCleared() {
